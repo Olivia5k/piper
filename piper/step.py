be9d2aa97ea7c913070318dc55876132a2929207
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-04-26 17:57:47 +0200

    Add skeleton for Step()

---
+++ piper/step.py
@@ -0,0 +1,45 @@
+import logbook
+
+from piper.utils import DotDict
+
+
+class Step(object):
+    """
+    An execution step.
+
+    This base implementation will execute whatever command line is set in the
+    configuration.
+
+    """
+
+    def __init__(self, conf, index):
+        self.conf = DotDict(conf)
+
+        # Index of execution order. Added by the runner when parsing the
+        # config file.
+        self.index = index
+
+        self.log = logbook.Logger(
+            '{}-{}'.format(self.__class__.__name__, self.index)
+        )
+
+    def execute(self):
+        """
+        The main entry point.
+
+        This is what Environment()s are supposed to run.
+
+        """
+
+        self.pre()
+        self.run()
+        self.post()
+
+    def pre(self):  # pragma: nocover
+        pass
+
+    def run(self):  # pragma: nocover
+        pass
+
+    def post(self):  # pragma: nocover
+        pass
---
+++ test/test_step.py
@@ -0,0 +1,19 @@
+from piper.step import Step
+
+import mock
+
+
+class TestStep(object):
+    def setup_method(self, method):
+        self.step = Step({}, 1)
+
+    def test_execute(self):
+        self.step.pre = mock.MagicMock()
+        self.step.run = mock.MagicMock()
+        self.step.post = mock.MagicMock()
+
+        self.step.execute()
+
+        self.step.pre.assert_called_once_with()
+        self.step.run.assert_called_once_with()
+        self.step.post.assert_called_once_with()
