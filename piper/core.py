a92a6a1ef81225f7c2257b5c8a203d69b25acf04
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-04-27 17:55:22 +0200

    Add skeleton of Piper()

---
+++ piper/core.py
@@ -0,0 +1,11 @@
+class Piper(object):
+    """
+    The main runner.
+
+    This class loads the configurations, sets up all other components, and
+    finally executes them in whatever order they are supposed to happen in.
+
+    """
+
+    def __init__(self):
+        pass
