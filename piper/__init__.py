62e3829e7caed34345218da50ff0bba2690aba4e
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-05-15 00:34:26 +0200

    Add actual script entry point :dash:

--- piper/__init__.py
+++ piper/__init__.py
@@ -1,2 +1,13 @@
+import sys
+
+from piper.core import Piper
+
+
 def main():
-    print('...come and fly away with me... <3')
+    # TODO: dat argparse
+    env_key = sys.argv[1]
+    set_key = sys.argv[2]
+
+    piper = Piper(env_key, set_key)
+    piper.setup()
+    piper.execute()
--- test/test_piper.py
+++ test/test_piper.py
@@ -1,7 +1,14 @@
 import piper
 
+import mock
+
 
 class TestPiper(object):
-    def test_main_entry_point(self):
-        # TODO: Actually implement <3
+    @mock.patch('piper.Piper')
+    @mock.patch('sys.argv')
+    def test_main_entry_point(self, argv, Piper):
         piper.main()
+
+        Piper.assert_called_once_with(argv[1], argv[2])
+        Piper.return_value.setup.assert_called_once_with()
+        Piper.return_value.execute.assert_called_once_with()
