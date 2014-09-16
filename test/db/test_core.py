4bf6a2fa24bb34f5e7c5b5766bfa7f62218567dc
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-09-17 00:51:57 +0200

    Move db tests from test_db to db/ test namespace

--- test/db/test_core.py
+++ test/db/test_core.py
@@ -3,6 +3,36 @@ from piper.db.core import LazyDatabaseMixin
 import mock
 import pytest
 
+from piper.db.core import DbCLI
+from piper.db.core import DatabaseBase
+
+
+class DbCLIBase(object):
+    def setup_method(self, method):
+        self.config = mock.Mock()
+        self.cli = DbCLI(self.config)
+        self.ns = mock.Mock()
+
+
+class TestDbCLIRun(DbCLIBase):
+    def test_plain_run(self):
+        self.cli.db.init = mock.Mock()
+        ret = self.cli.run(self.ns)
+
+        assert ret == 0
+        self.cli.db.init.assert_called_once_with(self.ns)
+
+
+class TestDatabaseBaseInit(object):
+    def setup_method(self, method):
+        self.db = DatabaseBase()
+        self.ns = mock.Mock()
+        self.config = mock.Mock()
+
+    def test_raises_not_implemented_error(self):
+        with pytest.raises(NotImplementedError):
+            self.db.init(self.ns, self.config)
+
 
 class TestLazyDatabaseMixinDb(object):
     def setup_method(self, method):
