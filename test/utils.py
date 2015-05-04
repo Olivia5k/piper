229bfad5ec7f6c167dd8c4384932cbcadc92a78c
Lowe Thiderman <lowe.thiderman@gmail.com>
2015-05-04 21:46:36 +0200

    Remove SQLATest fixture

--- test/utils.py
+++ test/utils.py
@@ -10,26 +10,6 @@ with open('piperd.yml') as f:
     AGENT_CONFIG = yaml.safe_load(f.read())
 
 
-class SQLATest(object):
-    """
-    Test base for tests that want a database setup
-
-    """
-
-    def setup_method(self, method):
-        # Just to avoid any future kind of circular dependencies
-        from piper.db.db_sqlalchemy import SQLAlchemyDB
-
-        self.db = SQLAlchemyDB()
-        self.config = mock.Mock()
-        self.config.raw = {
-            'db': {
-                'host': 'localhost',
-            }
-        }
-        self.build = mock.Mock()
-
-
 def builtin(target):
     """
     Return the correct string to mock.patch depending on Py3k or not.
